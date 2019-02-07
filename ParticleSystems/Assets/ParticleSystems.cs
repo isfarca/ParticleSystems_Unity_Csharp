5a4ef257dd35688522dd2bf7b3119d1d41289db1
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 21:55:31 +0100

    Adding project.

---
+++ ParticleSystems/Assets/ParticleSystems.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ParticleSystems : MonoBehaviour
+{
+    // Value types
+    private int randomNumber;
+
+    // Reference types
+    private ParticleSystem particleSystemEffect;
+
+    /// <summary>
+    /// Get the particle system component.
+    /// </summary>
+    private void Awake()
+    {
+        particleSystemEffect = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        // When the particle system stopped, than generate a random number.
+        if (particleSystemEffect.isStopped)
+        {
+            // Generate random number.
+            randomNumber = Random.Range(0, 100);
+            Debug.Log(randomNumber);
+
+            // When the random number is five, than play the particle system.
+            if (randomNumber == 5)
+                particleSystemEffect.Play();
+        }
+    }
+}
